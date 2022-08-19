c7545cd3194c1619952b931c5b8acf039f0c9ff5
magnuskjaer <70652804+magnuskjaer@users.noreply.github.com>
2022-08-19 08:40:49 +0200

    fix bug in test.sh script (#18)

--- functional-test/test.sh
+++ functional-test/test.sh
@@ -1,4 +1,3 @@
-# Test if the website is up
+# Test if the website is up & then Test if the website can connect to the backend
 curl -s localhost:8080 > /dev/null \
-# Test if the website can connect to the backend
 && curl localhost:8080/api/all
